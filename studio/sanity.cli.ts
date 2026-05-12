534ce891949f916bd49632ce91c218f36afd566f
Anwar Hegazy <anwarmhejazy@gmail.com>
2026-05-12 13:27:16 +0400

    Add Sanity deployment app ID

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -11,5 +11,6 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
      */
     autoUpdates: true,
+     appId: 'jto5h02lnq3dwv3ea0uy63a3',
   }
 })
